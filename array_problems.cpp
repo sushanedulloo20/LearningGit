d7ecdc0397e6f27838eb83af8063d8ecc490e50d
unknown <sushane21292@iiitd.ac.in>
2024-06-22 02:24:33 +0530

    My first commit

---
+++ array_problems.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<unordered_set>
+#include<vector>
+using namespace std;
+
+int main(){
+    unordered_set<int> s={1,3,2,4,9,6};
+    for(auto e:s){
+        cout<<e<<" ";
+    }
+    cout<<endl;
+    int num=2;
+    s.erase(num);
+    for(auto e:s){
+        cout<<e<<" ";
+    }
+    cout<<endl;
+    vector<int> v;
+    v
+
+    
+
+}
